0e175d8a191825fcfe6c9056d2cce63303b885af
Lyndsey Newsome <lyndseysnewsome@gmail.com>
2016-02-17 22:28:59 -0500

    L.Js1

--- Labyrinth.js
+++ Labyrinth.js
@@ -1,15 +1,67 @@
-prompt ("Your shirt is atrocious! I'm taking your dog! You'll have to solve my labyrinth. You have 13 mins. Pick your path! Left or Right! ");
-var side = "right";
-  if (side == "left") {
-    console.log("Boog of Eternal Strentch");
-  }
-    else if (side == "middle"){
-     console.log("Forest of Lost Memories");
-    }
-    else if (side == "right") {
-      console.log("City Gate");
+var side = prompt ("Your shirt is atrocious! I'm taking your dog! You'll have to solve my labyrinth. You have 13 mins. Pick your path! Left or Right! ");
+
+if (side == "left") {
+  alert("Boog of Eternal Strentch");
+
+  var boog_direction = prompt ("Oh! I see you're in the Boog of Eternal Stentch. Which way do you want to go. Left, or right. Your choice.");
+  var side;
+      if (side == "left") {
+          alert("My Rock friends help me across!I can go into the Forest.");
+      }
+      else (side == "right");{
+          alert("Oh No! I fell into the Bog of Eternal Stentch!");
+      }
+      var forest_direction = prompt ("I didn't think you'd get this far! You look hungry. Here have a peach?");
+         var eat;
+             if (eat == "Okay") {
+                 alert("Oh No! You can't remeber anything!");
+             }
+             else if (eat == "No") {
+                 alert( "Eww! I don't like peachse! I'll just keep going to the City.");
+             }
+             prompt ("So you're at the Gate. You don't desrve this dog! Do you give up");
+    var say;
+        if (say == "No") {
+            console.log("Whatever. Here's your dumb dog!");
+        }
+        else if (say == "Yes") {
+            console.log("You were never worthy of this dog!");
 }
-prompt ("Oh! I see you're in the Boog of Eternal Stentch. Which way do you want to go. Left, or right. Your choice.");
+
+};
+else if (side == "middle"){
+  alert("Forest of Lost Memories");
+  var forest_direction = prompt ("I didn't think you'd get this far! You look hungry. Here have a peach?");
+     var eat;
+         if (eat == "Okay") {
+             console.log("Oh No! I can't remeber anything!");
+         }
+         else if (eat == "No") {
+             console.log( "Eww! I don't like peachse! I'll just keep going to the City.");
+         }
+    var city_gate = prompt ("So you're at the Gate. You don't desrve this dog! Do you give up");
+             var say;
+                 if (say == "No") {
+                     console.log ("Whatever. Here's your dumb dog!");
+                 }
+                 else if (say == "Yes") {
+                     console.log("You were never worthy of this dog!");
+                 }
+
+}
+else if (side == "right") {
+  alert("City Gate");
+}
+var city_gate = prompt ("So you're at the Gate. You don't desrve this dog! Do you give up");
+    var say;
+        if (say == "No") {
+            console.log ("Whatever. Here's your dumb dog!");
+        }
+        else if (say == "Yes") {
+            console.log("You were never worthy of this dog!");
+        }
+
+
 var side;
     if (side == "left") {
         console.log("My Rock friends help me across!I can go into the Forest.");
@@ -18,16 +70,22 @@ var side;
         console.log("Oh No! I fell into the Bog of Eternal Stentch!");
     }
 
-
-prompt ("I didn't think you'd get this far! You look hungry. Here have a peach!");
+ var forest_direction = prompt ("I didn't think you'd get this far! You look hungry. Here have a peach?");
     var eat;
         if (eat == "Okay") {
             console.log("Oh No! You can't remeber anything!");
         }
         else if (eat == "No") {
             console.log( "Eww! I don't like peachse! I'll just keep going to the City.");
+            var city_gate =prompt ("So you're at the Gate. You don't desrve this dog! Do you give up");
+                var say;
+                    if (say == "No") {
+                        console.log ("Whatever. Here's your dumb dog!");
+                    }
+                    else if (say == "Yes") {
+                        console.log("You were never worthy of this dog!");
         }
-prompt ("So you're at the Gate. You don't desrve this dog! Do you give up");
+var city_gate = prompt ("So you're at the Gate. You don't desrve this dog! Do you give up");
     var say;
         if (say == "No") {
             console.log ("Whatever. Here's your dumb dog!");
